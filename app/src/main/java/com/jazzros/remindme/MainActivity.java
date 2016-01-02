eae216edf5056f16c22a20db356540ceb52e55d5
Oktay Radzhabov <oradzhabov@gmail.com>
2016-01-02 22:15:51 +0100

    Add Toolbar and search item

--- app/src/main/java/com/jazzros/remindme/MainActivity.java
+++ app/src/main/java/com/jazzros/remindme/MainActivity.java
@@ -2,14 +2,33 @@ package com.jazzros.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+//import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+//import android.support.v7.appcompat.R.
 
 /**
  * Created by Oktay on 02.01.2016.
  */
 public class MainActivity extends Activity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
