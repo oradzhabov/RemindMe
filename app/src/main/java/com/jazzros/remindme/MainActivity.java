567c8122742f056b8cf47775995d00b991100220
Oktay Radzhabov <oradzhabov@gmail.com>
2016-01-02 19:57:34 +0100

    Add MainActivity to project

---
+++ app/src/main/java/com/jazzros/remindme/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jazzros.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Oktay on 02.01.2016.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
