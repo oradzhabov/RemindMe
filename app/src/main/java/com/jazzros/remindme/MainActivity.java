335393d0332eb7d6b5125ae7f817ad5736d582a7
Oktay Radzhabov <oradzhabov@gmail.com>
2016-01-03 08:04:33 +0100

    Add NavigationBar (left panel)

--- app/src/main/java/com/jazzros/remindme/MainActivity.java
+++ app/src/main/java/com/jazzros/remindme/MainActivity.java
@@ -1,19 +1,19 @@
 package com.jazzros.remindme;
 
-import android.app.Activity;
 import android.os.Bundle;
-//import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-//import android.support.v7.appcompat.R.
+
 
 /**
  * Created by Oktay on 02.01.2016.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
